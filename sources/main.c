723add137743b7c161f61916ee4db1fdfad7598b
Eduardo Castro <95158753+educastrob@users.noreply.github.com>
2024-06-26 15:55:33 -0300

    Update main.c

--- sources/main.c
+++ sources/main.c
@@ -1,13 +1,1 @@
-int	main (int argc, char **argv)
-{
-  int		ret;
 
-  ret = 0;
-  if (argc == 1)
-	{
-	  my_putstr("Usage: ./my_ls [-lRdrt] [file ...]\n");
-	  return (0);
-	}
-  ret = my_ls(argc, argv);
-  return (ret);
-}
