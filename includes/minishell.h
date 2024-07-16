4f29acb07ce0bbf9e6d93c4dd4596c4489545a44
Eduardo Castro <eduardocastrobritoo@gmail.com>
2024-07-15 21:35:35 -0300

    add structs for lexing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 13:17:57 by educastro         #+#    #+#             */
-/*   Updated: 2024/06/26 15:49:46 by educastro        ###   ########.fr       */
+/*   Updated: 2024/07/15 16:44:34 by educastro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,49 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <signal.h>
+# include "fcntl.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+
+// enums
+enum e_token
+{
+	WORD,
+	PIPE,
+	REDIRECT_INPUT,
+	REDIRECT_HEREDOC,
+	REDIRECT_OUTPUT,
+	REDIRECT_OUTPUT_APPEND,
+	OR,
+	AND,
+	OPEN_PARENTHESIS,
+	CLOSE_PARENTHESIS,
+	EXPRESSION,
+	COMMAND,
+	SUBSHELL
+};
+
+// structs for lexing
+typedef struct		s_token
+{
+	char			*lexemes;
+	int				type;
+}	t_token;
+
+typedef struct		s_token_list
+{
+	t_token			token;
+	t_token_list	*next;
+}	t_token_list;
+
+
+
 
 #endif
