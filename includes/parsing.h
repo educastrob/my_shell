8c037e9867f1938c7f6b9c322d47f3aaca7d5d4a
Felipe Caldas Marques Nasser <fcaldas-@c1r10p4.42sp.org.br>
2024-07-24 19:17:59 -0300

    parsing header init

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,21 @@
 /*   By: fcaldas- <fcaldas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 18:19:00 by fcaldas-          #+#    #+#             */
-/*   Updated: 2024/07/24 18:19:01 by fcaldas-         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:17:31 by fcaldas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "minishell.h"
+
+typedef struct s_ast
+{
+	int				type;
+	char			*content;
+	struct s_ast	*left;
+	struct s_ast	*right;
+}	t_ast;
+
+#endif
