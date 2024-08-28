88a7a0ec51aac1b896c5aa0d7dfe1409f017c5ba
Eduardo Castro <95158753+educastrob@users.noreply.github.com>
2024-08-28 13:10:21 -0300

    Update env_func2.c

--- sources/expander/env_func2.c
+++ sources/expander/env_func2.c
@@ -6,61 +6,17 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 16:08:11 by edcastro          #+#    #+#             */
-/*   Updated: 2024/08/24 16:15:04 by edcastro         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:51:55 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "expander.h"
+#include "../../includes/expander.h"
 
-void	*free_env(t_env *env)
+t_env	*update_env(t_list *envs, char *name, char *value)
 {
-	free(env->name);
-	free(env->value);
-	free(env);
-	env = NULL;
-	return (env);
-}
-
-t_env	*init_actualise(t_env *env, char *name, char *value)
-{
-	t_env *new;
-
-	if (!(new = malloc(sizeof(t_env))))
-		return (new);
-	new->name = ft_strdup(name);
-	new->value = ft_strdup(value);
-	if (env && !ft_strncmp(env->name, name, ft_strlen(name)))
-	{
-		new->next = env->next;
-		free_env(env);
-		env = new;
-		return (NULL);
-	}
-	return (new);
-}
-
-int		actualise_env(t_env **env, char *name, char *value)
-{
-	t_env	*ret;
-	t_env	*new;
-
-	ret = *env;
-	if (!(new = init_actualise(*env, name, value)))
-		return (0);
-	while (env)
-	{
-		if ((*env)->next && !ft_strncmp((*env)->next->name, name,
-			ft_strlen(name)))
-		{
-			new->next = (*env)->next->next;
-			free_env((*env)->next);
-			(*env)->next = new;
-			break ;
-		}
-		env = &(*env)->next;
-	}
-	if (*env == NULL)
-		free_env(new);
-	*env = ret;
-	return (0);
+	t_env *ret = NULL;
+	(void)envs;
+	(void)name;
+	(void)value;
+	return ret;
 }
