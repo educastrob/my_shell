4573d13953d1237b86d417936f323220def2a362
Eduardo Castro <95158753+educastrob@users.noreply.github.com>
2024-08-28 13:12:18 -0300

    Update expander.h

--- includes/expander.h
+++ includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 18:37:12 by educastro         #+#    #+#             */
-/*   Updated: 2024/08/24 16:16:55 by edcastro         ###   ########.fr       */
+/*   Updated: 2024/08/28 13:12:06 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,26 @@
 
 // libs
 # include "minishell.h"
+#include "../libft/libft.h"
+
+typedef struct s_minishell t_minishell;
 
 // structs
 typedef struct	s_env
 {
-	char				*name;
-	char				*value;
-	struct s_env		*next;
-}	t_env;
-
+	char		*name;
+	char		*value;
+}				t_env;
 
 // env_func1.c
-int		add_env(t_env **env, char *name, char *value);
-int		init_env(t_env **env, char *envp[]);
-t_env	*get_env(t_env **env, char *name);
-char	*get_env_value(t_env **env, char *name);
-void	*free_all_env(t_env **env);
+t_env	*add_env(t_list *envs, char *name, char *value);
+t_list	*create_envs(char **envp);
 
 // env_func2.c
-void	*free_env(t_env *env);
-t_env	*init_actualise(t_env *env, char *name, char *value);
-int		actualise_env(t_env **env, char *name, char *value);
+t_env	*update_env(t_list *envs, char *name, char *value);
 
 // utils.c
-// t_env **my_env(t_env *env);
-
-
-// env_tests.c
-void print_env(t_env **env);
+t_minishell     *init_minishell(char **envp);
+int	print_env(t_minishell *minishell);
 
 #endif
