cb404682dde6a5d0b6a9b75423a554db4006111e
Eduardo Castro <95158753+educastrob@users.noreply.github.com>
2024-08-28 13:10:41 -0300

    Update utils.c

--- sources/expander/utils.c
+++ sources/expander/utils.c
@@ -6,18 +6,34 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 12:02:37 by edcastro          #+#    #+#             */
-/*   Updated: 2024/08/24 15:20:40 by edcastro         ###   ########.fr       */
+/*   Updated: 2024/08/28 13:08:01 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "expander.h"
+#include "../../includes/expander.h"
 
-// permite compartilhar uma unica instancia do env para o código inteiro.
-// t_env **my_env(t_env *env)
-// {
-// 	static t_env *env_total;
-	
-// 	if (env_total == NULL)
-// 		env_total = env;
-// 	return (&env_total);
-// }
+// inicializa o envp
+t_minishell     *init_minishell(char **envp)
+{
+    t_minishell *minishell;
+
+    minishell = ft_calloc(1, sizeof(t_minishell));
+    minishell->envs = create_envs(envp);
+    return (minishell);
+}
+
+int	print_env(t_minishell *minishell)
+{
+	t_list	*envs;
+	t_env	*env;
+
+	envs = minishell->envs;
+	while (envs != NULL)
+	{
+		env = envs->content;
+		if (env->value != NULL)
+			ft_printf("%s=%s\n", env->name, env->value);
+		envs = envs->next;
+	}
+	return (EXIT_SUCCESS);
+}
