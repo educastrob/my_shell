7de80553678bafbef081d48ffede6e075f217894
Eduardo Castro <eduardocastrobritoo@gmail.com>
2024-09-26 18:49:09 -0300

    describes on signals marcos added

--- sources/executor/signals_macros.c
+++ sources/executor/signals_macros.c
@@ -6,33 +6,38 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:00:14 by edcastro          #+#    #+#             */
-/*   Updated: 2024/09/26 17:14:08 by edcastro         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:42:23 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 
+// verifica se o processo foi encerrado por um sinal
 int	term_by_signal(int status)
 {
 	return (((signed char)((status & 0x7f) + 1) >> 1) > 0);
 }
 
+// verifica se o processo foi encerrado normalmente
 int	term_normaly(int status)
 {
 	return (term_by_signal(status) == 0);
 }
 
+// obtém o código de saída do processo
 int	exit_status(int status)
 {
 	return ((status >> 8) & 0xFF);
 }
 
+// obtém o número do sinal que encerrou o processo
 int	term_signal(int status)
 {
 	return (status & 0x7F);
 }
 
+// obtém o valor de retorno a ser usado pela função exit()
 int	get_return_value(int status)
 {
 	if (term_by_signal(status))
